c0dbdf1ba9725ae93bec5a1813950d4343614b86
guhansomasundaram <guhan56sgg@gmail.com>
2025-01-05 23:03:18 +0530

    Including -swagger-ui -Rest assured -Spotless maven -Docker compose -GitHub actions

--- catalog-service/src/test/java/com/bookstore/catalog_service/TestCatalogServiceApplication.java
+++ catalog-service/src/test/java/com/bookstore/catalog_service/TestCatalogServiceApplication.java
@@ -2,13 +2,13 @@ package com.bookstore.catalog_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @PropertySource(value = "application-dev.yml")
 public class TestCatalogServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
 }
